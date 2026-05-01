74f75014561df3d4cdf3b0418ff1565ed4748fbd
Christopher Lovelace <61518654+ChrisLovelace@users.noreply.github.com>
2026-05-01 11:57:38 -0400

    Update calendar_.py
    Updated file comment

--- tkcalendar/calendar_.py
+++ tkcalendar/calendar_.py
@@ -20,6 +20,9 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
+-- With one small change by Christopher Lovelace <chris.lovelace16@gmail.com>
+    Allows for DDMMMYYY date format
+
 Calendar widget
 """
 
